efd7b294405ee4a0beda4718baaea1667452e0e5
Yusuf <93403926+YusufHoDYavuz@users.noreply.github.com>
2022-11-09 21:25:30 +0300

    added crash and ragdoll system

---
+++ Assets/Scripts/Ragdoll.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Ragdoll : MonoBehaviour
+{
+    Animator anim;
+    NavMeshAgent playerAgent;
+    PathMover pathMover;
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+        playerAgent = GetComponent<NavMeshAgent>();
+        pathMover = GetComponent<PathMover>();
+
+        RGRigidbody(true);
+        RDCollider(true);
+    }
+
+    void RGRigidbody(bool status)
+    {
+        Rigidbody[] rg = GetComponentsInChildren<Rigidbody>();
+        foreach (Rigidbody rbChild in rg)
+        {
+            rbChild.isKinematic = status;
+        }
+    }
+
+    void RDCollider(bool status)
+    {
+        Collider[] cl = GetComponentsInChildren<Collider>();
+        foreach (Collider clChild in cl)
+        {
+            clChild.enabled = status;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        RagdollActive();
+    }
+
+    void RagdollActive()
+    {
+        anim.enabled = false;
+        playerAgent.enabled = false;
+        pathMover.enabled = false;
+        RGRigidbody(false);
+        RDCollider(true);
+    }
+}
