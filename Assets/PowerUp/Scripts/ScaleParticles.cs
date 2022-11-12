c32fa27de9c523f62793bc45c2e1a71a1b4536a0
Yusuf <93403926+YusufHoDYavuz@users.noreply.github.com>
2022-11-12 14:31:59 +0300

    added clock system - counter and animations

--- Assets/PowerUp/Scripts/ScaleParticles.cs
+++ Assets/PowerUp/Scripts/ScaleParticles.cs
@@ -4,6 +4,8 @@ using System.Collections;
 [ExecuteInEditMode]
 public class ScaleParticles : MonoBehaviour {
 	void Update () {
-		GetComponent<ParticleSystem>().startSize = transform.lossyScale.magnitude;
-	}
+#pragma warning disable CS0618 // Type or member is obsolete
+        GetComponent<ParticleSystem>().startSize = transform.lossyScale.magnitude;
+#pragma warning restore CS0618 // Type or member is obsolete
+    }
 }
--- Assets/Scripts/TimeCounter.cs
+++ Assets/Scripts/TimeCounter.cs
@@ -1,30 +1,54 @@
 using UnityEngine;
+using DG.Tweening;
 using System.Collections;
 using UnityEngine.UI;
 
 public class TimeCounter : MonoBehaviour
 {
     [SerializeField] private Text count;
-    private int counter;
+    [SerializeField] private int clockActiveDelay;
+    [SerializeField] private GameObject AddedTimePanel;
+
+    private int secondCounter;
 
     void Start()
     {
-        StartCoroutine(Counter(5));
+        StartCoroutine(SecondCounter(0));
     }
 
-    void Update()
+    IEnumerator SecondCounter(int startTime)
     {
-    }
+        secondCounter = startTime;
 
-    IEnumerator Counter(int seconds)
-    {
-        counter = seconds;
-
-        while (counter > -1)
+        while (secondCounter > -1)
         {
-            count.text = "0: " + count.ToString();
+            count.text = secondCounter.ToString();
             yield return new WaitForSeconds(1);
-            counter++;
+            secondCounter++;
         }
     }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.transform.CompareTag("Clock"))
+        {
+            secondCounter += 5;
+            StartCoroutine(DeactiveAndActiveClock(clockActiveDelay, other.gameObject));
+        }
+    }
+
+    IEnumerator DeactiveAndActiveClock(int activeDelay, GameObject gameObject)
+    {
+        gameObject.transform.DOScale(Vector3.zero, 0.5f);
+        gameObject.transform.DOLocalRotate(Vector3.zero, 0.5f).OnComplete(()=> {
+            gameObject.SetActive(false);
+        });
+        AddedTimePanel.transform.DOMoveY(1920, 0.75f).SetEase(Ease.InOutBack);
+        yield return new WaitForSeconds(2);
+        AddedTimePanel.transform.DOMoveY(2160, 0.5f).SetEase(Ease.InOutBack);
+        yield return new WaitForSeconds(activeDelay);
+        gameObject.SetActive(true);
+        gameObject.transform.DOScale(new Vector3(0.2f, 0.2f, 0.2f), 0.5f);
+    }
+
 }
