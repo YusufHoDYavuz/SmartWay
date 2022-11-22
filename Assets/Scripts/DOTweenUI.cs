ed30a67f6736e6d3ad8c664ed6ecd0f40c7fecaf
Yusuf <93403926+YusufHoDYavuz@users.noreply.github.com>
2022-11-22 12:18:58 +0300

    added taptostart

---
+++ Assets/Scripts/DOTweenUI.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class DOTweenUI : MonoBehaviour
+{
+    [SerializeField] private GameObject tapToStartPanel;
+    [SerializeField] private GameObject pathCreator;
+    [SerializeField] private GameObject timeCounter;
+    [SerializeField] private float tapToStartSpeed;
+
+    void Start()
+    {
+        tapToStartPanel.transform.DOScale(new Vector3(0.9f, 0.9f, 0.9f), tapToStartSpeed).SetEase(Ease.InOutSine).SetLoops(-1, LoopType.Yoyo);
+    }
+
+    // CALLED BY BUTTON
+    public void StartGame()
+    {
+        tapToStartPanel.SetActive(false);
+        pathCreator.SetActive(true);
+        timeCounter.SetActive(true);
+    }
+}
